d65ff3e28ec33637d3d6a6643a6b7d7c05ec8c08
Devonte Renshaw <devonte@devonterenshaw.com>
2026-09-21 20:37:26 -0700

    Add Supabase secrets management via .env

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,13 @@
+import os
+
+from dotenv import load_dotenv
 from fastapi import FastAPI
 
+
+load_dotenv()
+
+SUPABASE_URL = os.environ["SUPABASE_URL"]
+SUPABASE_SECRET_KEY = os.environ["SUPABASE_SECRET_KEY"]
 
 app = FastAPI()
 
